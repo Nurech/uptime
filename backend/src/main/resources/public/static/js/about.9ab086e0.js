dc7c35effd9d95f49a86940a73805ba3419de4cb
Joosep <joosep.parts@gmail.com>
2021-05-16 20:59:16 +0300

    adding eslint

---
+++ backend/src/main/resources/public/static/js/about.9ab086e0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about"},[a("h3",[t._v("Made by")]),a("h",[t._v("Joosep Parts")]),a("br"),a("a",{attrs:{href:"https://github.com/Nurech/uptime"}},[t._v("Find project on Github")])],1)},s=[],u=a("2877"),o={},r=Object(u["a"])(o,n,s,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.9ab086e0.js.map
